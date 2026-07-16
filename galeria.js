785d5a5a811b3828a016ea5c920d92ad64405313
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-16 11:08:46 -0300

    Update image loading logic in galeria.js
    Refactor image loading to use data from Firestore instead of local JSON.

--- galeria.js
+++ galeria.js
@@ -14,65 +14,24 @@ if(!galeria) return;
 
 galeria.innerHTML = "";
 
-let imagenesMostradas = new Set();
-
-function agregarImagen(url, titulo){
-
-if(imagenesMostradas.has(url)) return;
-
-imagenesMostradas.add(url);
-
-const img = document.createElement("img");
-
-img.src = url;
-
-img.alt = titulo || "Leitmix Producciones";
-
-galeria.appendChild(img);
-
-}
-
-
-// Galería antigua
-
-try{
-
-const respuesta = await fetch("./content/galeria.json?v=4");
-
-const imagenes = await respuesta.json();
-
-imagenes.forEach(item=>{
-
-agregarImagen(item.image,item.title);
-
-});
-
-}catch(error){
-
-console.log(error);
-
-}
-
-
-// Firebase
-
-try{
 
 const fotos = await getDocs(collection(db,"galeria"));
 
-fotos.forEach(foto=>{
+
+fotos.forEach((foto)=>{
 
 const datos = foto.data();
 
-agregarImagen(datos.url,datos.titulo);
+const img = document.createElement("img");
+
+img.src = datos.url;
+
+img.alt = datos.titulo || "Leitmix Producciones";
+
+galeria.appendChild(img);
 
 });
 
-}catch(error){
-
-console.log(error);
-
-}
 
 }
 
