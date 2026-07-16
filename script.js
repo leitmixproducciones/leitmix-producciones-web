3281bf18e443ead8e24e076f30d42283342f8ad2
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-16 20:05:36 -0300

    Uncomment cargarGaleria function call

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 import { supabase } from "./firebase.js";
 
 
-// CARGAR CONTENIDO DE LA WEB (por ahora desactivado hasta crear tabla contenido)
-
-
 // GALERIA SUPABASE
 
 async function cargarGaleria(){
