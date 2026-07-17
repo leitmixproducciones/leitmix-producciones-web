9a804a8c2a9ba792ab14225ee9ba68a75ac70e98
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-17 11:28:30 -0300

    Implement video saving feature
    Added functionality to save video details to Supabase.

--- admin.js
+++ admin.js
@@ -1,6 +1,8 @@
 import { supabase } from "./supabase.js";
 
 
+// GUARDAR IMAGEN
+
 const imagenUrl = document.getElementById("imagenUrl");
 const imagenTitulo = document.getElementById("imagenTitulo");
 const botonImagen = document.getElementById("guardarImagen");
@@ -32,6 +34,46 @@ alert("Imagen guardada correctamente");
 
 imagenUrl.value="";
 imagenTitulo.value="";
+
+
+};
+
+
+
+
+// GUARDAR VIDEO
+
+const videoUrl = document.getElementById("videoUrl");
+const videoTitulo = document.getElementById("videoTitulo");
+const botonVideo = document.getElementById("guardarVideo");
+
+
+botonVideo.onclick = async function(){
+
+
+const { error } = await supabase
+.from("videos")
+.insert([
+{
+Titulo: videoTitulo.value,
+URL: videoUrl.value
+}
+]);
+
+
+if(error){
+
+alert(error.message);
+return;
+
+}
+
+
+alert("Video guardado correctamente");
+
+
+videoUrl.value="";
+videoTitulo.value="";
 
 
 };
