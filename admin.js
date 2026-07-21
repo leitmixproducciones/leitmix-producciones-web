948a5659b12e9f2bc06fc33288dc208be48bb3c4
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-21 19:47:03 -0300

    Redirect unauthenticated users to login page

--- admin.js
+++ admin.js
@@ -1,5 +1,10 @@
 import { supabase } from "./supabase.js";
 
+const { data } = await supabase.auth.getSession();
+
+if(!data.session){
+    window.location.href="admin/login.html";
+}
 
 // ======================
 // CONFIGURACIÓN DEL NEGOCIO
