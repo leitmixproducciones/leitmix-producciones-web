2750c4c689aed1246132b768472344fbaa403873
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-21 22:24:59 -0300

    Remove duplicate session check and redirect

--- admin.js
+++ admin.js
@@ -3,10 +3,6 @@ import { supabase } from "./supabase.js";
 
 const { data } = await supabase.auth.getSession();
 
-if(!data.session){
-
-window.location.href = "./login.html";
-
 
 if(!data.session){
 
