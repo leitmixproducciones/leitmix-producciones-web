8588caa86f75f68a9631567e794cf820a7e89c33
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-21 23:53:46 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,19 @@
-const CACHE_NAME = "leitmix-panel-v1";
+const CACHE_NAME = "leitmix-panel-v2";
 
 const ARCHIVOS = [
+  "admin/login.html",
   "admin/panel.html",
   "admin.js",
   "supabase.js",
-  "assets/img/leitmix-logo.jpeg"
+  "assets/img/leitmix-logo.jpeg",
+  "manifest.json"
 ];
 
 
+// Instalar nueva versión
 self.addEventListener("install", event => {
+
+  self.skipWaiting();
 
   event.waitUntil(
 
@@ -20,16 +25,43 @@ self.addEventListener("install", event => {
 });
 
 
+// Activar y borrar caché vieja
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+
+            return caches.delete(key);
+
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+// Buscar archivos
 self.addEventListener("fetch", event => {
 
   event.respondWith(
 
-    caches.match(event.request)
-    .then(response => {
-
-      return response || fetch(event.request);
-
-    })
+    fetch(event.request)
+    .catch(()=>caches.match(event.request))
 
   );
 
