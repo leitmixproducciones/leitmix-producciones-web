c654621d70ae465f4916235dde6889be5856f1b6
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-27 19:29:12 -0300

    Add general configuration for LEITMIX PRODUCCIONES

---
+++ config.js
@@ -0,0 +1,8 @@
+// ==========================================
+// CONFIGURACIÓN GENERAL DE LEITMIX PRODUCCIONES
+// ==========================================
+export const CONFIG = {
+    telefonoWhatsApp: "5491150480339", // Poné tu número con código de país y área sin espacios ni símbolos
+    nombreEmpresa: "LEITMIX PRODUCCIONES",
+    slogan: "Sonido, Iluminación y DJs"
+};
