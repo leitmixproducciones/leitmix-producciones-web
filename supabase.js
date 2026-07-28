bf12b6b6918c4baa687ed3a8a74acf0fcf09fb6e
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-28 19:49:45 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-const SUPABASE_URL = "https://fevvtbbyzxvnanzeedzp.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_l8Hb4ydFb7uGcjODFG8sBg_jY-jEFrF";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
