941609c93bd0035d4445304edc794d46aa4e560f
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-28 12:14:59 -0300

    Update Supabase anon key in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 const SUPABASE_URL = "https://fevvtbbyzxvnanzeedzp.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZldnZ0YmJ5enh2bmFuemVlZHpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMzAwNjIsImV4cCI6MjA5OTgwNjA2Mn0.Fy8OcP0Bx9h1cK4sX3UR1pVYSk3pmOh2nrUco5-F_5k";
+const SUPABASE_ANON_KEY = "sb_publishable_l8Hb4ydFb7uGcjODFG8sBg_jY-jEFrF";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
