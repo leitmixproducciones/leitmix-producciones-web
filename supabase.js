ce1c2148ad8f183a858b74ef03b9deac368729a3
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-28 23:40:12 -0300

    Update Supabase URL and anon key

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = 'https://fevvtbbyzxvnanzeedzp.supabase.co';
-const SUPABASE_ANON_KEY = 'Sb_publishable_l8Hb4ydFb7uGcjODFG8sBg_jY-jEFrF';
+const SUPABASE_URL = 'https://frmtmjonzwdpdzpuhfdw.supabase.co';
+const SUPABASE_ANON_KEY = 'Sb_publishable_1GqwIW44gpYMvHby2-Aw6Q_qouik6Ax';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
