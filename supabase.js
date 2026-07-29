7f0c61d2c0935dbfeb20b2a7b5408e40d8407ab2
LEITMIX PRODUCCIONES <leitmixproducciones@gmail.com>
2026-07-29 00:56:50 -0300

    Change Supabase key to a new value
    Updated the Supabase key to a new value.

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://frmtmjonzwdpdzpuhfdw.supabase.co'
-const supabaseKey = 'Sb_publishable_1GqwIW44gpYMvHby2-Aw6Q_qouik6Ax'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZybXRtam9uendkcGR6cHVoZmR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyODQyNjgsImV4cCI6MjEwMDg2MDI2OH0.EQw6_5fBbm2Vl55cDjfgK2T0-aDzUSjecd8rlssvhxw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
